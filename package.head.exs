8c94896a4bfd279875bf7817235bb94b88402476
Vladimir Kirillov <proger@wilab.org.ua>
2013-08-31 18:59:46 +0300

    added expm package

---
+++ package.head.exs
@@ -0,0 +1,5 @@
+Expm.Package.new(name: "active", description: "Active development for Erlang: rebuild and reload source/binary files while the VM is running",
+                 version: :head, keywords: ["Erlang", "sync", "reload", "active development"], 
+                   maintainers: [[name: "Vladimir Kirillov", email: "proger@hackndev.com"]],
+                     repositories: [[github: "proger/active"]],
+                     dependencies: [])
